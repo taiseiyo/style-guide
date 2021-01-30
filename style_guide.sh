0c3d704c7b58c07be314850097a5b3de51319690
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2021-01-30 19:47:31 +0900

    fix

---
+++ style_guide.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+declare -A urls=(["ShellScript"]="https://google.github.io/styleguide/shellguide.html"
+		 ["Python"]="https://pep8-ja.readthedocs.io/ja/latest/"
+		 ["JavaScript"]="https://developer.mozilla.org/ja/docs/Mozilla/JavaScript_style_guide"
+		)
+
+for key in ${!urls[@]};do
+    echo ${key}
+done | fzf | { read v; firefox ${urls[${v}]};}
